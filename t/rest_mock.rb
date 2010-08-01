9a319dbf9aeebe71a2ce72d38b1c865423c1923e
Aliaksey Kandratsenka <alk@tut.by>
2010-07-31 21:23:46 -0700

    made rest_mock.rb stream for real
    The idea of using Rack::Evil to workaround something in sinatra that
    prevents streaming support is taken from:
    http://amberbit.com/blog/ruby-flv-pseudostreaming
    
    Change-Id: I251992862d67cfecfe56ca8c592913261658e4c4

--- t/rest_mock.rb
+++ t/rest_mock.rb
@@ -7,26 +7,59 @@
 #   ruby ./t/cluster_manager_mock.rb ./t/vbucket1.cfg
 #
 require 'rubygems'
+gem 'sinatra'
+gem 'rack-contrib'
+
 require 'sinatra'
+require 'rack/contrib'
 
-get '/pools/default/buckets/default' do
-  some_json
-end
 
-get '/pools/default/bucketsStreaming/default' do
-  some_json + "\n\n\n\n"
-end
-
-get '/pools/default/bucketsStreamingConfig/default' do
-  some_json + "\n\n\n\n"
-end
-
-def some_json()
-  if ARGV.length > 0
-    return File.read(ARGV[0])
+class Application < Sinatra::Base
+  get '/pools/default/buckets/default' do
+    some_json
   end
 
-  <<-eos
+  get '/pools/default/bucketsStreaming/default' do
+    stream do
+      some_json + "\n\n\n\n"
+    end
+  end
+
+  get '/pools/default/bucketsStreamingConfig/default' do
+    stream do
+      some_json + "\n\n\n\n"
+    end
+  end
+
+  class InfiniteStream
+    def initialize(block)
+      @block = block
+    end
+    def each
+      while true
+        rv = @block.call
+        break unless rv
+        yield rv
+      end
+    end
+  end
+
+  def stream(&block)
+    first_time = true
+    resp = InfiniteStream.new(lambda do
+                                sleep 5 unless first_time
+                                first_time = false
+                                block.call
+                              end)
+    throw :response, [200, {'Content-Type' => 'application/json'}, resp]
+  end
+
+  def some_json()
+    if ARGV.length > 0
+      return File.read(ARGV[0])
+    end
+
+    <<-eos
 {
   "name": "TheUser",
   "saslPassword": "ThePassword",
@@ -41,5 +74,15 @@ def some_json()
   }
 }
   eos
+  end
 end
 
+$app = Rack::Builder.app do
+  use Rack::CommonLogger
+  use Rack::Evil
+  run Application
+end
+
+config = {:app => $app, :Port => 4567}
+handler = Rack::Handler.default(config)
+handler.run $app, config
