e49d2155c2760e154b84faee1199fb0748ee0549
Aliaksey Kandratsenka <alk@tut.by>
2010-10-16 12:56:47 -0700

    mark
    Change-Id: I64bdf3a087f082604954b4e06ad5be39abd1966a

--- memcached.c
+++ memcached.c
@@ -3723,7 +3723,7 @@ void event_handler(const int fd, const short which, void *arg) {
     thread = c->thread;
 
     while (thread->pending_writes_count > 0) {
-        drive_machine(thread->pending_writes[--thread->pending_writes_count]);
+        drive_machine(thread->pending_writes[--(thread->pending_writes_count)]);
     }
 
     /* wait for next event */
